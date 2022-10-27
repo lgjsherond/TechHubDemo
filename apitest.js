9ed66c4a399694461ef6ad084c8bb22380fbb569
lgjsherond <50234665+lgjsherond@users.noreply.github.com>
2022-10-27 13:20:32 +0530

    wileyTechhub codes

---
+++ apitest.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.2/index.js';
+
+export default function testSuite() {
+  describe('Fetch a list of public crocodiles', () => {
+    const response = http.get('https://test-api.k6.io/public/crocodiles');
+
+    expect(response.status, 'response status').to.equal(200);
+    expect(response).to.have.validJsonBody();
+    expect(response.json().length, 'number of crocs').to.be.above(4);
+  });
+}
